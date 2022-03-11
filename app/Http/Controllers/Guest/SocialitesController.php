3a06540eac1914415e1f716d80a21d50f9139f10
Genesis <dudesaranyu@gmail.com>
2022-03-11 12:08:05 +0700

    socialites successfully

--- app/Http/Controllers/Guest/SocialitesController.php
+++ app/Http/Controllers/Guest/SocialitesController.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
 use App\Models\Subscribe;
+use App\Models\Order;
 use App\Models\UserVerify;
 
 class SocialitesController extends Controller
@@ -25,6 +26,50 @@ class SocialitesController extends Controller
     public function callback($provider)
     {
         $socialite = Socialite::driver($provider)->user();
-        dd($socialite);
+
+        $user = User::where('email', $socialite->email)->first();
+
+        if (!$user) {
+            $user = new User();
+            $user->full_name = $socialite->name;
+            $user->email = $socialite->email;
+            $user->email_verified_at = Carbon::now();
+            $user->password = Hash::make(Str::random(40));
+            $user->role = 'User';
+            $user->status = 'Active';
+            $user->save();
+
+            $subscribe = Subscribe::where('email', $socialite->email)->first();
+            if ($subscribe) {
+                $subscribe->user_id = $user->id;
+                $subscribe->save();
+            } else {
+                $subscribe = new Subscribe();
+                $subscribe->email = $socialite->email;
+                $subscribe->status = 'Enabled';
+                $subscribe->user_id = $user->id;
+                $subscribe->save();
+            }
+
+            $orders = Order::where('email', $socialite->email)->get();
+            foreach ($orders as $key => $order) {
+                $order->user_id = $user->id;
+                $order->save();
+            }
+
+            $userVerify = new UserVerify();
+            $userVerify->token = Str::random(40);
+            $userVerify->user_id = $user->id;
+            $userVerify->save();
+        } else {
+            if ($user->email_verified_at == '') {
+                $user->email_verified_at = Carbon::now();
+                $user->save();
+            }
+        }
+
+        Auth::login($user);
+
+        return redirect()->route('/');
     }
 }
